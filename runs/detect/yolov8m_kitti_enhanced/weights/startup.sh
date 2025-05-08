e7b962ad92fe277cfe5efeed3ae596084dce915e
Mohamed-Ibrahim20 <mohamadcr28@gmail.com>
2025-05-09 02:57:26 +0300

    Changes to be committed: 	modified:   runs/detect/yolov8m_kitti_enhanced/weights/startup.sh

--- runs/detect/yolov8m_kitti_enhanced/weights/startup.sh
+++ runs/detect/yolov8m_kitti_enhanced/weights/startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/site/wwwroot
 cp -f runs/detect/yolov8m_kitti_enhanced/weights/main.py app.py
+cp -f runs/detect/yolov8m_kitti_enhanced/weights/best.pt best.pt
 mkdir -p uploads static templates
 pip install -r runs/detect/yolov8m_kitti_enhanced/weights/requirements.txt
 gunicorn --bind=0.0.0.0 --timeout 600 app:app 
