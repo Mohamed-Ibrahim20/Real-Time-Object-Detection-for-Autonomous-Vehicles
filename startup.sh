301922d5ee3e37bc3509ce9b5b31f43b295b9e7b
Mazen Amir <49759150+Selvster@users.noreply.github.com>
2025-05-09 03:58:31 +0300

    Statup.sh

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set up virtual environment
+python3 -m venv /home/site/wwwroot/antenv
+
+# Activate the virtual environment
+source /home/site/wwwroot/antenv/bin/activate
+
+# Install dependencies from requirements.txt
+pip install -r /home/site/wwwroot/requirements.txt
+
+# Start the application using Gunicorn or your desired server
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker app:app --bind=0.0.0.0 --timeout 600
